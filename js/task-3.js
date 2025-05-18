0299484787766f970418aab5a0a54e8601756e77
Olena Hryhorzhevska <elenahry8@gmail.com>
2025-05-17 21:01:11 -0400

    hw done

--- js/task-3.js
+++ js/task-3.js
@@ -1,27 +1,7 @@
-class StringBuilder {
-  #value;
-  constructor(initialValue) {
-    this.#value = initialValue;
-  }
-  getValue() {
-    return this.#value;
-  }
-  padEnd(str) {
-    this.#value += str;
-  }
-  padStart(str) {
-    this.#value = str + this.#value;
-  }
-  padBoth(str) {
-    this.#value = str + this.#value + str;
-  }
-}
+const nameInput = document.querySelector("#name-input");
+let nameOutput = document.querySelector("#name-output");
 
-const builder = new StringBuilder(".");
-console.log(builder.getValue()); // "."
-builder.padStart("^");
-console.log(builder.getValue()); // "^."
-builder.padEnd("^");
-console.log(builder.getValue()); // "^.^"
-builder.padBoth("=");
-console.log(builder.getValue()); // "=^.^="
+nameInput.addEventListener('input', (event) => {
+  const trimmedValue = event.currentTarget.value.trim();
+  nameOutput.textContent = trimmedValue === "" ? "Anonymous" : trimmedValue;
+})
